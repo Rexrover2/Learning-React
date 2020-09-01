ffa22beb09e07145c74dae2c2792cacf80a9ce60
Lawrence Leong <lsleong@student.unimelb.edu.au>
2020-09-01 14:54:15 +1000

    Successfully Converted from utilising classes to utilising  hooks now!

--- src/index.js
+++ src/index.js
@@ -2,82 +2,75 @@ import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Square(props) {
+const Square = props => {
+    return (
+      <button
+        className="square"
+        onClick={props.onClick}
+      >
+        {props.value}
+      </button>
+    );
+};
+  
+const Board = props => {    
+    const renderSquare = i => {
       return (
-        <button 
-            className="square" 
-            onClick={props.onClick}
-        >
-          {props.value}
-        </button>
+        <Square
+          value={props.squares[i]}
+          onClick={() => props.onClick(i)}
+        />
       );
-  }
+    };
   
-class Board extends React.Component {
-    renderSquare(i) {
-        return (
-            <Square
-                value={this.props.squares[i]}
-                onClick={() => this.props.onClick(i)}
-            />
-        );
-    }
-    
-    render() {
-        return (
-            <div>
-                <div className="board-row">
-                {this.renderSquare(0)}
-                {this.renderSquare(1)}
-                {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                {this.renderSquare(3)}
-                {this.renderSquare(4)}
-                {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                {this.renderSquare(6)}
-                {this.renderSquare(7)}
-                {this.renderSquare(8)}
-                </div>
-            </div>
-        );
-    }
-}
+    return (
+      <div>
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+    );
+};
   
-function Game(props) {
-    const [history, setHistory] = useState( {history: { 
-                                                squares: [Array(9).fill(null)] 
-                                            }});
+const Game = (props) => {
+    const initialHistory = [
+            { squares: Array(9).fill(null) }
+        ]
+    const [history, setHistory] = useState(initialHistory);
     const [xIsNext, setXIsNext] = useState(true);
     const [stepNumber, setStepNumber] = useState(0);
 
-    const updateHistory = (squares) => {
-        const temp = history.slice(0, stepNumber+1);
-        temp.concat([{
-            squares: squares
-        }])
-        setHistory(temp);
-    } 
+    const handleClick = (i) => {
+        const slicedHistory = history.slice(0, stepNumber+1);
+        const current = slicedHistory[slicedHistory.length - 1];
+        const newSquares = current.squares.slice();
 
-    function handleClick(i) {
-        const revertHistory = history.slice(0, stepNumber+1);
-        const current = revertHistory[revertHistory.length - 1];
-        const squares = current.squares.slice();
-
-        if (calculateWinner(squares) || squares[i]) {
+        // Nothing happens if square is already filled and there is no winner.
+        if (calculateWinner(newSquares) || newSquares[i]) {
             return;
         }
-        squares[i] = xIsNext ? 'X' : 'O';
-        updateHistory(squares);
-        setStepNumber(history.length);
+
+        newSquares[i] = xIsNext ? 'X' : 'O';
+        setHistory([...slicedHistory, {squares: newSquares}]);
+        setStepNumber(slicedHistory.length);
         setXIsNext(!xIsNext);
     }
     
     const jumpTo = (step) => {
         setStepNumber(step) 
-        setXIsNext((step % 2)==0)
+        setXIsNext((step % 2)===0)
     };
 
     // Renders Buttons
@@ -92,39 +85,43 @@ function Game(props) {
         )
     });
     
-    const current = history[stepNumber];
+    const currentStep = history[stepNumber];
+    const winner = calculateWinner(currentStep.squares);
 
-    // Check if game state!
+    /* // How to have a conditional instead of the ternary operator below??
     const getStatus = () => {
-        const winner = calculateWinner(current.squares);
-        
-        let status;
+        const winner = calculateWinner(currentStep.squares);
+
         if (winner==='tied') {
-            return 'Game ' + winner;
+            return <div>{'Game' + winner}</div>;
         } else if (winner) {
-            return 'Winner: ' + winner;
+            return <div>{'Winner: ' + winner}</div>;
         } else {
-            return 'Next Player: ' + (xIsNext)
+            return <div>{'Next Player: ' + (xIsNext)}</div>;
         }
-    };
+    }; */
+
+    const getStatus = winner
+    ? `Winner: ${winner}`
+    : `Next player: ${xIsNext ? 'X' : 'O'}`;
 
     return (
         <div className="game">
             <div className="game-board">
             <Board 
-                squares={current.squares}
-                onClick={(i) => handleClick(i)}
+                squares={currentStep.squares}
+                onClick={i => handleClick(i)}
             />
             </div>
             <div className="game-info">
-            <div>{ getStatus }</div>
+            <div> {getStatus} </div>
             <ol>{ moves }</ol>
             </div>
         </div>
     );
 }
   
-function calculateWinner(squares) {
+const calculateWinner = (squares) => {
     const lines = [
         [0,1,2],
         [3,4,5],
@@ -135,14 +132,14 @@ function calculateWinner(squares) {
         [0,4,8],
         [2,4,6]
     ];
-    var numFull = 0;
-    for (let i = 0; i< lines.length; i++){
-        const [a,b,c] = lines[i];
+    
+    for (let line of lines){
+        const [a,b,c] = line;
         if (squares[a] && squares[a]===squares[b] && squares[a]===squares[c]) {
             return squares[a];
         }    
     }
-
+    var numFull = 0;
     for (let i=0; i<9; i++) {
         numFull += (squares[i]!=null ? 1 : 0);
     }
